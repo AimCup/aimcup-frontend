4119830305fc70277d79bbe3cd54e000d1940b9b
Patryk <orzeleagle122@gmail.com>
2024-06-03 16:24:34 +0200

    feat: add open api

--- src/app/(main-page)/dashboard/page.tsx
+++ src/app/(main-page)/dashboard/page.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+// import { UserService } from '../../../../generated'
 
 const Dashboard = () => {
+	// const handler = async () => {
+	// 	const data = await UserService.me();
+	// 	console.log(data);
+	// };
+
 	return (
 		<div>
 			<h1>Dashboard</h1>
