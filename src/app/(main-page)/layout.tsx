e5dc0b3f00ed8bc82b9142d9a11648f8a83d1ea6
Adam Orłowski <orlowski.adam.99@gmail.com>
2024-06-17 12:59:26 +0200

    feat: added loading bar

--- src/app/(main-page)/layout.tsx
+++ src/app/(main-page)/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import NextTopLoader from "nextjs-toploader";
 import { Navbar } from "@ui/organisms/Navbar/Navbar";
 import { Footer } from "@ui/organisms/Footer/Footer";
 
 export default function MainPageLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
+			<NextTopLoader color="#CA191B" height={5} showSpinner={false} />
 			<Navbar />
 			<div className={""}>{children}</div>
 			<Footer />
