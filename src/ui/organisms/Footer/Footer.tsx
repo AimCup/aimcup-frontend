891142601a025aab2058bfd62c39567dc6e9048b
Adam Orłowski <orlowski.adam.99@gmail.com>
2024-06-17 12:59:00 +0200

    feat: fetch tournaments in footer

--- src/ui/organisms/Footer/Footer.tsx
+++ src/ui/organisms/Footer/Footer.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 import { SiKofi } from "react-icons/si";
 import { FaDiscord, FaTwitch } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import Link from "next/link";
+import { TournamentService } from "../../../../generated";
 
 export const Footer = async () => {
+	const tournaments = await TournamentService.getTournaments();
+
 	return (
 		<footer className={"md:px-18 md:py-18 px-8 py-10 lg:px-20 lg:py-20"}>
 			<div className={"container mx-auto"}>
@@ -27,24 +31,27 @@ export const Footer = async () => {
 						<p>Socials</p>
 						<p>My profile</p> {/*TODO: Ukryte gdy użytkownik niezalogowany*/}
 					</div>
-					{/*TODO: Nazwy turniejów fetchowane z API*/}
 					<div className={"flex-grow flex-col space-y-3 max-sm:hidden sm:hidden md:flex"}>
-						<p>AimCup</p>
-						<p>AimCup 2</p>
-						<p>AimCup Sidequest</p>
-						<p>AimCup 3</p>
+						{tournaments.map((tournament) => (
+							<Link
+								key={tournament.id}
+								href={`/tournament/${tournament.abbreviation?.toLowerCase()}`}
+							>
+								<p key={tournament.id}>{tournament.name}</p>
+							</Link>
+						))}
 					</div>
 					<div className={"flex flex-wrap justify-center gap-14"}>
 						<div
 							className={
-								"bg-primary-light flex h-16 w-16 cursor-pointer items-center justify-center rounded-full transition-opacity hover:opacity-80"
+								"flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-primary-light transition-opacity hover:opacity-80"
 							}
 						>
 							<SiKofi size={"32px"} color={"#151120"} />
 						</div>
 						<div
 							className={
-								"bg-primary-light flex h-16 w-16 cursor-pointer items-center justify-center rounded-full transition-opacity hover:opacity-80"
+								"flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-primary-light transition-opacity hover:opacity-80"
 							}
 						>
 							<FaTwitch size={"32px"} color={"#151120"} />
@@ -52,14 +59,14 @@ export const Footer = async () => {
 
 						<div
 							className={
-								"bg-primary-light flex h-16 w-16 cursor-pointer items-center justify-center rounded-full transition-opacity hover:opacity-80"
+								"flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-primary-light transition-opacity hover:opacity-80"
 							}
 						>
 							<FaDiscord size={"32px"} color={"#151120"} />
 						</div>
 						<div
 							className={
-								"bg-primary-light flex h-16 w-16 cursor-pointer items-center justify-center rounded-full transition-opacity hover:opacity-80"
+								"flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-primary-light transition-opacity hover:opacity-80"
 							}
 						>
 							<FaXTwitter size={"32px"} color={"#151120"} />
@@ -67,7 +74,7 @@ export const Footer = async () => {
 					</div>
 				</div>
 				<div className={"flex flex-nowrap content-center items-center py-10"}>
-					<p className={"text-tuned flex-grow"}>© Aim Cup 2024</p>
+					<p className={"flex-grow text-tuned"}>© Aim Cup 2024</p>
 					<div>
 						<p className={"text-tuned"}>Designed by</p>
 						<p className={"font-bold text-deepRed"}>starlightof</p>
