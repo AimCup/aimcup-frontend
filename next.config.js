6af262fd35f165812be6c3f3b7cb33726bffbfd4
Adam Orłowski <orlowski.adam.99@gmail.com>
2024-06-19 22:04:08 +0200

    feat: add ppy.sh to remote patterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "standalone",
+	images: {
+		remotePatterns: [{ protocol: "https", hostname: "a.ppy.sh", port: "", pathname: "/**/*" }],
+	},
 };
 
 module.exports = nextConfig;
