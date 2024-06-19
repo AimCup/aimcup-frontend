fb7574089fea17fe4eaff44ac2f3fb009e95e613
Patryk <orzeleagle122@gmail.com>
2024-06-19 19:04:20 +0200

    chore: fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
-import { CookiesProvider } from "next-client-cookies/server";
 import { OpenAPI, UserService } from "../../generated";
 import StoreProvider from "@/app/StoreProvider";
 import UserProvider from "@/app/UserProvider";
@@ -24,17 +23,15 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
 	return (
 		<StoreProvider>
-			<CookiesProvider>
-				<UserProvider userData={data}>
-					<html
-						lang="en"
-						className="scroll-smooth bg-primary-dark text-primary-light"
-						style={{ scrollBehavior: "smooth" }}
-					>
-						<body className={inter.className}>{children}</body>
-					</html>
-				</UserProvider>
-			</CookiesProvider>
+			<UserProvider userData={data}>
+				<html
+					lang="en"
+					className="scroll-smooth bg-primary-dark text-primary-light"
+					style={{ scrollBehavior: "smooth" }}
+				>
+					<body className={inter.className}>{children}</body>
+				</html>
+			</UserProvider>
 		</StoreProvider>
 	);
 }
